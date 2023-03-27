299a00f50b4bfef58af2e1ae5db26306e708002d
stepkr <krutinas@spsstav.cz>
2023-03-27 11:19:54 +0200

    (Semi)Final

--- script.js
+++ script.js
@@ -5,16 +5,18 @@ function kap() {
     let delka = document.getElementById("delka").value;
     let cas = document.getElementById("cas").value;
 
+    document.getElementById("vypsatMaterial").innerText = lambda;
+
     let sVysledek = document.getElementById("sVysledek").value
 
-    let vysledek = lambda * ((plocha * deltaT * cas)/delka)
+    let vysledek = (lambda * ((plocha * deltaT * cas)/delka))/1000
     console.log(vysledek)
 
     if (checkbox.checked) {
          if (vysledek == sVysledek) {
-        document.getElementById("hodnoceni").innerText = "Skvěle! Q = " + vysledek + " J je správný výsledek."
+        document.getElementById("hodnoceni").innerText = "Skvěle! Q = " + vysledek + " kJ je správný výsledek."
         } else {
-        document.getElementById("hodnoceni").innerText = "Ne ne, tohle není správná odpověď. Měl(a) ses dobrat k výsledku Q = " + vysledek + " J."
+        document.getElementById("hodnoceni").innerText = "Ne ne, tohle není správná odpověď. Měl(a) ses dobrat k výsledku Q = " + vysledek + " kJ."
         }
     } else {
         if (vysledek == sVysledek) {
@@ -25,6 +27,7 @@ function kap() {
     }
 }
 
+
 function vypocet() {
     let lambda =  document.getElementById("lambda").value;
     let plocha = document.getElementById("plocha").value;
@@ -32,7 +35,7 @@ function vypocet() {
     let delka = document.getElementById("delka").value;
     let cas = document.getElementById("cas").value;
 
-    let vysledek2 = lambda * ((plocha * deltaT * cas)/delka)
+    let vysledek2 = (lambda * ((plocha * deltaT * cas)/delka))/1000
     console.log(vysledek2)
 
     document.getElementById("vysledek").innerText = vysledek2
